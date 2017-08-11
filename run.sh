ded3c932827a46abae650e3560402473cdcee0f1
Laurence MacGuire <leprechaun@gmail.com>
2017-08-11 13:20:20 +0700

    rename the host to the pod (with sequence id!)

--- run.sh
+++ run.sh
@@ -41,6 +41,7 @@ common(){
 	curl -v -X POST http://127.0.0.1:8091/pools/default -d memoryQuota=300 -d indexMemoryQuota=300
 	curl -v http://127.0.0.1:8091/node/controller/setupServices -d services=kv%2Cn1ql%2Cindex
 	curl -v http://127.0.0.1:8091/settings/web -d port=8091 -d username=$USERNAME -d password=$PASSWORD
+	curl -v -X POST -u $USERNAME:$PASSWORD http://127.0.0.1:8091/node/controller/rename -d hostname=${HOSTNAME}
 	curl -i -u $USERNAME:$PASSWORD -X POST http://127.0.0.1:8091/settings/indexes -d 'storageMode=memory_optimized'
 	set +e
 }
