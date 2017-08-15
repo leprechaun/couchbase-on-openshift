3bfb9a3631bcc959814cd38e2199385c3fba49ea
Laurence MacGuire <leprechaun@gmail.com>
2017-08-15 21:27:59 +0700

    hmmm. dns search path issues?

--- run.sh
+++ run.sh
@@ -77,7 +77,7 @@ worker(){
 	common
 
 	set -x
-	couchbase-cli rebalance --cluster=${STATEFULSET_NAME}-0.${STATEFULSET_NAME}.${NAMESPACE}.svc.cluster.local --user=$USERNAME --password=$PASSWORD --server-add=$FQDN --server-add-username=$USERNAME --server-add-password=$PASSWORD
+	couchbase-cli rebalance --cluster=${STATEFULSET_NAME}-0.${STATEFULSET_NAME}.${NAMESPACE}.svc --user=$USERNAME --password=$PASSWORD --server-add=$FQDN --server-add-username=$USERNAME --server-add-password=$PASSWORD
 	set +x
 
   echo 1 > /tmp/ready
